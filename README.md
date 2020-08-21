b1153110dafa62438162f903852c57afc077f16a
Grant Eadie <grantleadie@gmail.com>
2020-08-21 11:28:42 -0700

    refactor the code for simpler look.

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
             * INPUT- 4
             * OUTPUT- "0, 1, 2, 3, 4"
 
-      *     the programs replaces all 1 digits with _"Beep!"_
-            * INPUT- 5
+      *     the program replaces all 1 digits with _"Beep!"_
+            * INPUT- 4
             * OUTPUT- "0, Beep!, 2, 3, 4"
+
+      *     the program replaces all 2 digits with _"Boop!"_
+            * INPUT- 4
+            * OUTPUT- "0, BEEP!, BOOP!, 4"
--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,31 @@
+// Business
+
 function robogers(userInput) {
     let array = [];
+    let newArray = [];
+    let lastArray = [];
     for (let i = 0; i <= userInput; i++){
-      array.push(i);
+      array.push(i.toString());
+      newArray.push(((array[i]).split(''))[0]);
+      if (newArray[i] === "1") {
+        lastArray.push("Beep!");
+      } else if (newArray[i] === "2") {
+        lastArray.push("Boop!");
+      } else if (newArray[i] === "3") {
+        lastArray.push("Won't you be my neighbor?");
+      } else {
+        lastArray.push(array[i]);
+      }
     }
-    return array;
-}
+    return lastArray.join(' ');
+  }
 
-
+// Interface
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
       event.preventDefault();
-    let userInput = parseInt($("input#userInput").val());
-    
+    let userInput = $("input#userInput").val();
     let result = robogers(userInput);
     $("#output").text(result);
   });
