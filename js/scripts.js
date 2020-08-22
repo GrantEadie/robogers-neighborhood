a56cc1976c01ff6cda56d2eca8eeb6b08a067712
Grant Eadie <grantleadie@gmail.com>
2020-08-22 10:38:25 -0700

    refactors code.

--- js/scripts.js
+++ js/scripts.js
@@ -3,39 +3,21 @@
 function robogers(userInput) {
     let array = [];
     let newArray = [];
-    let lastArray = [];
     for (let i = 0; i <= userInput; i++){
       array.push(i.toString());
-    }
-    for (let e = 0; e < array.length; e++) {
-      if ((array[e]).includes("3")) {
+      if ((array[i]).includes("3")) {
         newArray.push("Won't you be my neighbor?");
-      } else if (array[e].includes("2")) {
+      } else if (array[i].includes("2")) {
         newArray.push('Boop!');
-      } else if (array[e].includes("1")) {
+      } else if (array[i].includes("1")) {
         newArray.push('Beep!');
       } else {
-        newArray.push(array[e]);
+        newArray.push(array[i]);
       }
     }
     console.log(newArray);
     return newArray.join(' ');
   }
-      
-  //     newArray.push(((array[i]).split(''))[0]);
-  //     alert(newArray[i]);
-  //     if (newArray[i] === "3") {
-  //       lastArray.push("Beep!");
-  //     } else if (newArray[i] === "2") {
-  //       lastArray.push("Boop!");
-  //     } else if (newArray[i] === "1") {
-  //       lastArray.push("Won't you be my neighbor?");
-  //     } else {
-  //       lastArray.push(array[i]);
-  //     }
-  //   }
-  //   return lastArray.join(' ');
-  // }
 
 // Interface
 
