5cb28ac83921994fdfe371661cd781f4fcd1f6e4
Grant Eadie <grantleadie@gmail.com>
2020-08-30 17:23:27 -0700

    fix spacing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 // Business
 
 function robogers(userInput) {
-    let array = [];
-    let newArray = [];
-    for (let i = 0; i <= userInput; i++){
-      array.push(i.toString());
-      if ((array[i]).includes("3")) {
-        newArray.push("Won't you be my neighbor?");
-      } else if (array[i].includes("2")) {
-        newArray.push('Boop!');
-      } else if (array[i].includes("1")) {
-        newArray.push('Beep!');
-      } else {
-        newArray.push(array[i]);
-      }
+  let array = [];
+  let newArray = [];
+  for (let i = 0; i <= userInput; i++){
+    array.push(i.toString());
+    if ((array[i]).includes("3")) {
+      newArray.push("Won't you be my neighbor?");
+    } else if (array[i].includes("2")) {
+      newArray.push('Boop!');
+    } else if (array[i].includes("1")) {
+      newArray.push('Beep!');
+    } else {
+      newArray.push(array[i]);
     }
-    console.log(newArray);
-    return newArray.join(' ');
   }
+  console.log(newArray);
+  return newArray.join(' ');
+}
 
 // Interface
 
